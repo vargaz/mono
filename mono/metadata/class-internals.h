3c5daa6b878b534d04710fbb23fd5fb3c8ca6dca
Zoltan Varga <vargaz@gmail.com>
2012-03-19 20:17:23 +0100

    .

--- mono/metadata/class-internals.h
+++ mono/metadata/class-internals.h
@@ -718,6 +718,7 @@ typedef struct {
 	gulong generics_sharable_methods;
 	gulong generics_unsharable_methods;
 	gulong generics_shared_methods;
+	gulong gsharedvt_methods;
 	gulong minor_gc_count;
 	gulong major_gc_count;
 	gulong minor_gc_time_usecs;
